777a7f91a46b39637ea15f6f48792a626fb60e20
CrazyKrabbyPatty <CrazyKrabbyPatty@gmail.com>
2025-04-20 23:02:34 +0300

    Add files via upload

--- src/pages/CreateProduct/CreateProduct.jsx
+++ src/pages/CreateProduct/CreateProduct.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from "../../component/UI/Navbar/Navbar";
 import classes from "./CreateProduct.module.css";
+import ButtonBlock from "../../component/UI/button/ButtonBlock";
 
 const CreateProduct = () => {
+    const [ProductName, setProductName] = useState("");
+    const [ProductDescription, setProductDescription] = useState("");
+    const [cost, setCost] = useState("");
+    const [category, setCategory] = useState("");
+    const [image, setImage] = useState("");
+
+    const Create_Product = async (event) => {
+        event.preventDefault();
+        console.log(event);
+    }
+
     return (
-        <body className={classes.CreateProduct_body}>
+        <div className={classes.CreateProduct_body}>
             <Navbar />
 
-            <main className={classes.CreateProduct_main}>
-                <div className={classes.column}>
-                    <p className={classes.text_big}>Создание товара</p>
-                    <img src='#' alt='Изображение недоступно' className={classes.image_placeholder}/>
-                    <p className={classes.text_big}>Категория товара</p>
-                </div>
-                <div className={classes.column}>
+            <main>
 
-                </div>
+                <form onSubmit={Create_Product} className={classes.CreateProduct_main}>
+
+                    <div className={classes.first_column}>
+                        <p className={classes.text_big}>Создание товара</p>
+                        <img src='#' alt='Изображение недоступно' className={classes.image_placeholder}/>
+                        <p className={classes.text_big}>Категория товара</p>
+                    </div>
+
+                    <div className={classes.second_column}>
+                        <div className={classes.ProductName_Publish}>
+                            <input
+                                className={classes.ProductName}
+                                type="text"
+                                placeholder="Введите название товара"
+                                value={ProductName}
+                                onChange={(e) => setProductName(e.target.value)}
+                            />
+
+                            <button className={classes.button} type="submit">
+                                Опубликовать
+                            </button>
+                        </div>
+                    </div>
+                </form>
+
             </main>
-        </body>
+        </div>
     );
 };
 
